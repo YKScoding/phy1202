b255ddf928dbdc29845c9e83892313bc0f121b46
YKScoding <82931936+YKScoding@users.noreply.github.com>
2023-10-07 13:59:59 +0800

    Update A5Q4.pas

--- A5Q4.pas
+++ A5Q4.pas
@@ -1,13 +1,13 @@
 
 program Phy_Q4;
 var
-    k , d : extended;
-    qa , qb , F , m , a , s: extended;
-    ans : extended;
+    k , work : extended;
+    qa , qb , F , m , ra , rb : extended;
+    v : extended;
 begin
     k := 8.99 * 1000000000;
     writeln('input distance of charge 2');
-    readln(s);
+    readln(rb);
     writeln('input the magnitute of charge 1');
     readln(qa);
     writeln('input the magnitute of charge 2');
@@ -15,13 +15,10 @@ begin
     writeln('input the mass of the 2nd point charge');
     readln(m);
     writeln('distance to origin of which the speed is measured');
-    readln(d);
+    readln(ra);
     qa := qa * 0.000001;
     qb := qb * 0.000001;
     m := m * 0.0001;
-    F := (k * qa * qb) / sqr(s-d) ;
-    a := F / m;
-    writeln('The accelaration is ' , a:8:6);
-    ans := sqrt(Abs(2 * a * (s-d)));
-    writeln('the answer is ' , ans:8:6 );
+    v := sqrt(abs(2 * (k * qa * qb) * ((1 / rb) - (1 / ra)) / m));
+    writeln('the answer is ' , v:8:6 );
 end.
