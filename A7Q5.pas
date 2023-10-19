498c34f1d2c022b7301211ae72f63f7f1729d63c
YKScoding <82931936+YKScoding@users.noreply.github.com>
2023-10-19 10:37:35 +0800

    Update A7Q5.pas

--- A7Q5.pas
+++ A7Q5.pas
@@ -9,21 +9,11 @@ begin
     R := R * power(10, -6);
     d := d / 1000;
     
-
-    
-    Vn := ohms / R;
-    {volume needed  divided by area = length}
-    writeln('The volume needed is ' , Vn:16:16 , ' m^3');
-    Area := pi * power((d/2) , 2);
-    writeln('The area of the wire is ' , Area:16:16 , ' m^2');
-    length := Vn / Area;
-    writeln('The length of wire needed is: ' , length:8:8 , ' m.');
     
     {Resistivity per area = R * R}
     {Length needed to achieve desired resistance = res Desired/ res per Area}
-    length := ohms / (R*R);
+    Area := pi * power((d/2) , 2);
+    length := ohms * Area / R;
     writeln;
-    writeln('iteration 2: ');
-    writeln('The length of wire needed is: ' , length:8:8 , ' m.')
-
+    writeln('The length of wire needed is: ' , length:8:8 , ' m.');
 end.
