7471ee1586dd203f26596162d022b96f6245f734
YKScoding <82931936+YKScoding@users.noreply.github.com>
2023-10-26 10:31:02 +0800

    Update A8Q1.pas

--- A8Q1.pas
+++ A8Q1.pas
@@ -2,16 +2,19 @@
 
 program phy_calc_1;
 var
-    r1,r2,r3,r123,r4,v,i,r1234:extended;
+    r1,r2,r3,r123,r4,v,i,r :extended;
 begin
     writeln('input resistor 1,2,3,4, and voltage');
     readln(r1,r2,r3,r4,v);
     {1/r123 = 1/r1 + 1/r2 ...}
     r123 := 1/r1 + 1/r2 + 1/r3;
     r123 := 1/R123;
-    r1234 := r123 + r4;
+    r := r123 + r4;
     {v =ir}
-    i := v/r1234;
-    writeln('the answer is ',i:8:8)
+    i := v/r; {calculating i for all circuit}
+    v := i * r123; {i is constant for series, calculate v for p resistor}
+    i := v/r3; {voltage is constant for parallel, calc i for r3}
+    writeln(v:3:3, ' V');
+    writeln('the answer is ',i:8:8);
+    writeln(r3:3:3, ' ohm')
 end.
-
