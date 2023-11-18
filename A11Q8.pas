41d2533132c3e4f2e5fb579a565166c8462887d0
YKScoding <82931936+YKScoding@users.noreply.github.com>
2023-11-18 18:03:29 +0800

    Update A11Q8.pas

--- A11Q8.pas
+++ A11Q8.pas
@@ -8,9 +8,7 @@ uses
 var
     n,{turns per unit area}
     time,
-    t,
     flux,
-    target,{target b field}
     B,
     r,{radius}
     u0,{constant}
@@ -20,22 +18,17 @@ var
 begin
     u0:=4*pi*power(10,-7);
     writeln('input number of turns, B-field increase to, time, and radius.');
-    readln(n,target,time,r);
+    readln(n,B,time,r);
     
     {B:=u0*i*n;}
-    t:=0;
-    B:=0;
-    repeat begin
-    B:=target;
     A:=pi*sqr(r);
-    flux:=B*A;
-    B:=B+0.1;
-    t:=t+0.1;
-    end;
-    until (B = target) or (t = time);
-    
+    flux:=B*A;{-(B_initial*A), which is 0}
+    {emf = -delta(N*Flux)/deltaT}
+    n:=n/A;
+    V:=(flux*n)/time;
+    {-N can be abs()'ed since the question is asking for magnitude only.}
     {TODO: make it correct...}
     
-    writeln('The flux is: ',flux:6:6,' . ')
+    writeln('The flux is: ',flux:6:6,' . ');
+    writeln('The emf is: ',V:6:6,' V. ')
 end.
-
