02db44ac5796c88b30a33c329ce7a21a26510dce
YKScoding <82931936+YKScoding@users.noreply.github.com>
2023-11-20 15:00:31 +0800

    Update A11Q9.pas

--- A11Q9.pas
+++ A11Q9.pas
@@ -27,18 +27,16 @@ begin
     r:=r/2; {change diameter to radius}
     r:=r/100;  {change radius into meter}
     B:=B*power(10,-3);  {change mT to T}
+    d:=d/100;
+    incB:=incB*power(10,-3);
     {B = u0*i*n}
     
     
-    t:=1; {set the time to an arbituary 1 second.}
-    n:=100/(pi*sqr(r));{set turns to an arbituary 100}
-    i:=B/(u0*n);
+ 
     
-    {iinc:=incB/(u0*n);{current increase in 1 second}
-    
-    A:=pi*sqr(r);
-    emf:=-A*incB{/s, which is 1}
-    {E:=(r/2)*B*(iinc-i);}
+
+    {emf:=-A*incB/s, which is 1}
+    E:=(1/2)*(incB)*(sqr(r)/d);
     
     E:=E*power(10,3);
     
